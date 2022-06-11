79dc7332233fc820dd05d76a91865275a6a1a6b6
adxell <adxell02@gmail.com>
2022-06-11 02:33:48 -0500

    update source

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
           Hi👋, my name is Adxell, i'm student at universidad simon bolivar in
           the systems engineering program
         </p>
-        <p>This is my personal blog for my english class😎😎😎</p>
+        <p>This is my personal blog for my english class😎</p>
         <div className="container">
           <Section
             color={"#D3EBCD"}
