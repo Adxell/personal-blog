0b53aecd597473a10ba951bf8df8ec713a7e15c3
adxell <adxell02@gmail.com>
2022-06-17 12:45:10 -0500

    Entry 4

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ function App() {
   const styleBackground = {
     color: "#000",
     background: "#fff",
-    height:"100vh"
-
+    height: "100vh",
   };
   const contentEntry2 = (
     <div>
@@ -57,6 +56,256 @@ function App() {
       </p>
     </div>
   );
+  const contentEntry4 = (
+    <>
+      <h3></h3>
+      <div className="container">
+        <ul>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Homeopathy</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Homeopatía</h6>
+                <p>
+                  Homeopathy or homoeopathy is a pseudoscientific system of
+                  alternative medicine. Its practitioners, called homeopaths,
+                  believe that a substance that causes symptoms of a disease in
+                  healthy people can cure similar symptoms in sick people; this
+                  doctrine is called similia similibus curentur, or "like cures
+                  like".
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Acupuncture</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Acupuntura</h6>
+                <p>
+                  Acupuncture is a form of alternative medicine and a component
+                  of traditional Chinese medicine in which thin needles are
+                  inserted into the body. Acupuncture is a pseudoscience; the
+                  theories and practices of TCM are not based on scientific
+                  knowledge, and it has been characterized as quackery.
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Herbs</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Hierbas</h6>
+                <p>
+                  In general use, herbs are a widely distributed and widespread
+                  group of plants, excluding vegetables and other plants
+                  consumed for macronutrients, with savory or aromatic
+                  properties that are used for flavoring and garnishing food,
+                  for medicinal purposes, or for fragrances.
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Smoking</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Fumar</h6>
+                <p>
+                  Smoking is a practice in which a substance is burned and the
+                  resulting smoke is typically breathed in to be tasted and
+                  absorbed into the bloodstream.
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Herbal Therapy</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Terapia de hierbas</h6>
+                <p>
+                  Herbal medicine is the study of pharmacognosy and the use of
+                  medicinal plants, which are a basis of traditional medicine.
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Health</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Salud</h6>
+                <p>
+                  Health is a state of complete physical, mental and social
+                  well-being and not merely the absence of disease or infirmity.
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Medicine</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Medicamento</h6>
+                <p>
+                  Medicines are chemicals or compounds used to cure, halt, or
+                  prevent disease; ease symptoms; or help in the diagnosis of
+                  illnesses.
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Natural</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Natural</h6>
+                <p>
+                  existing in or derived from nature; not made or caused by
+                  humankind.
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Body</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Cuerpo</h6>
+                <p>
+                  the physical structure, including the bones, flesh, and
+                  organs, of a person or an animal.
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Problems</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Problemas</h6>
+                <p>
+                  a matter or situation regarded as unwelcome or harmful and
+                  needing to be dealt with and overcome.
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Remedies</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Remedios</h6>
+                <p>
+                  a medicine, application, or treatment that relieves or cures a
+                  disease.
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Pills</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Pastillas</h6>
+                <p>are a kind of medicine with hormones</p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Diluted</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Diluida</h6>
+                <p>
+                  To make a liquid weaker by mixing it with water or another
+                  liquid
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Religions</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Religiones</h6>
+                <p>
+                  Religion is usually defined as a social-cultural system of
+                  designated behaviors and practices, morals, beliefs,
+                  worldviews, texts, sanctified places, prophecies, ethics, or
+                  organizations.
+                </p>
+              </div>
+            </li>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <li>
+              <h5>Cultures</h5>
+              <div style={{ display: "block", justifyContent: "center" }}>
+                <h6>Translate to spanish Culturas</h6>
+                <p>
+                  Culture is an umbrella term which encompasses the social
+                  behavior, institutions, and norms found in human societies, as
+                  well as the knowledge, beliefs, arts, laws, customs,
+                  capabilities, and habits of the individuals in these groups.
+                </p>
+              </div>
+            </li>
+          </div>
+        </ul>
+
+        <div>
+          <h2>Differences</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Conventional Medicine</th>
+                <th>Homeopathy</th>
+                <th>Acupuncture</th>
+                <th>Spiritual Healing</th>
+                <th>Herbal Therapy</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  Conventional medicine is a system in which medical doctors and
+                  other healthcare professionals (such as nurses, pharmacists
+                  and therapists) treat symptoms and diseases using drugs,
+                  radiation or surgery.
+                </td>
+                <td>
+                  Homeopathy or homoeopathy is a pseudoscientific system of
+                  alternative medicine. Its practitioners, called homeopaths,
+                  believe that a substance that causes symptoms of a disease in
+                  healthy people can cure similar symptoms in sick people; this
+                  doctrine is called similia similibus curentur, or "like cures
+                  like".
+                </td>
+                <td>
+                  Acupuncture involves the insertion of very thin needles
+                  through your skin at strategic points on your body. A key
+                  component of traditional Chinese medicine, acupuncture is most
+                  commonly used to treat pain.
+                </td>
+                <td>
+                  Spiritual healing is a systematic and purposeful intervention
+                  performed by one or more people to help another person,
+                  focused on improving their conditions.
+                </td>
+                <td>
+                  Herbal medicine is the study of pharmacognosy and the use of
+                  medicinal plants, which are a basis of traditional medicine.
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
   return (
     <div className="App" style={styleBackground}>
       <div
@@ -84,7 +333,18 @@ function App() {
             content={contentEntry2}
             day={"2"}
           />
-          <Section color={"#FAF4B7"} title={"Table manners"} content={contentEntry3} day={"3"} />
+          <Section
+            color={"#FAF4B7"}
+            title={"Table manners"}
+            content={contentEntry3}
+            day={"3"}
+          />
+          <Section
+            color={"#3AB0FF"}
+            title={"Medicine"}
+            content={contentEntry4}
+            day={"4"}
+          />
         </div>
       </div>
     </div>
